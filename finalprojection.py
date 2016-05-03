3107d93d463a1b69f302e320752aa1d0b2f30d3b
adamglueck <adam.glueck1@gmail.com>
2016-05-03 14:30:25 -0400

    Create finalprojection.py

---
+++ finalprojection.py
@@ -0,0 +1,47 @@
+a=0
+b=0
+c=0
+d=0
+e=0
+f=0
+g=0
+h=0
+i=0
+k=0
+while k=0:
+    if a==b and b==c and c==1:
+        k=1
+        winner='x'
+    if a==b and b==c and c==2:
+        k=1
+        winner='o'
+    if d==e and e==f and f==1:
+        k=1
+        winner='x'
+    if d==e and e==f and f==2:
+        k=1
+        winner='o'
+    if g==h and h==i and f==1:
+        k=1
+        winner='x'
+    if g==h and h==i and f==2:
+        k=1
+        winner='o'
+    if a==d and d==g and g==1:
+        k=1
+        winner='x'
+    if a==d and d==g and g==2:
+        k=1
+        winner='o'
+    if b==e and e==h and h==1:
+        k=1
+        winner='x'
+    if b==e and e==h and h==2:
+        k=1
+        winner='o'
+    if c==f and f==i and i==1:
+        k=1
+        winner='x'
+    if c==f and f==i and i==2:
+        k=1
+        winner='o'
