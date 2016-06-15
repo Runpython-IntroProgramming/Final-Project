750bcfd5d028621497a51ccd7f23f4ccc4edecb0
Suhan Gui <suhangui@hanovernorwichschools.org>
2016-06-14 22:19:45 -0400

    Updated from Brython Server: 6/14/2016 10:19:33 PM

--- experimental.py
+++ experimental.py
@@ -1,1 +1,27 @@
+from ggame import App, Color, Sprite, RectangleAsset, LineStyle, TextAsset, MouseEvent, CircleAsset
 
+red= Color(0xff0000, 1)
+green= Color(0x228b22, 1)
+black= Color(0X000000, 1)
+outline= LineStyle(1, black)
+nooutline= LineStyle(0, red)
+
+makepix= []
+
+def create():
+    for celle in makepix[:]:
+        Cell((celle[0], celle[1]))
+        makepix.remove(celle)
+
+class thechoice(Sprite):
+    
+    strings = {'single': 'Single Player',
+        'multi': '2 Players',
+        }
+    
+    def __init__(self, position):
+        super().__init__(thechoice.dia, position)
+        
+        self.listenMouseEvent('click', self.decide)
+
+        if 
