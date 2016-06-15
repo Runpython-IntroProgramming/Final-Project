811517a77aa194713d56f25df24d1036acfe682a
Suhan Gui <suhangui@hanovernorwichschools.org>
2016-06-14 20:57:11 -0400

    Updated from Brython Server: 6/14/2016 8:56:59 PM

--- getrekthagin.py
+++ getrekthagin.py
@@ -1,5 +1,10 @@
 from ggame import App, Color, Sprite, RectangleAsset, LineStyle, MouseEvent
 
+red= Color(0xff0000, 1)
+green= Color(0x228b22, 1)
+black= Color(0X000000, 1)
+
+def thechoice():
 
 myapp = tictactoe(640, 480)
 myapp.run()
