b87cc9e7bd9342b312a2d500591a9e1b6d15a0b5
Suhan Gui <suhangui@hanovernorwichschools.org>
2016-06-14 21:13:59 -0400

    Updated from Brython Server: 6/14/2016 9:13:47 PM

--- getrekthagin.py
+++ getrekthagin.py
@@ -1,10 +1,16 @@
-from ggame import App, Color, Sprite, RectangleAsset, LineStyle, MouseEvent
+from ggame import App, Color, Sprite, RectangleAsset, LineStyle, TextAsset
 
 red= Color(0xff0000, 1)
 green= Color(0x228b22, 1)
 black= Color(0X000000, 1)
 
-def thechoice():
+class thechoice:
+    
+    strings = {'single': 'Single Player!',
+        'multi': '2 Players!',
+        }
+
+class tictactoe(App):
 
 myapp = tictactoe(640, 480)
 myapp.run()
