63d181b2163d270efd9b0f11b569b60d052e742d
Suhan Gui <suhangui@hanovernorwichschools.org>
2016-06-14 21:33:35 -0400

    Updated from Brython Server: 6/14/2016 9:33:22 PM

--- getrekthagin.py
+++ getrekthagin.py
@@ -3,6 +3,8 @@ from ggame import App, Color, Sprite, RectangleAsset, LineStyle, TextAsset, Mous
 red= Color(0xff0000, 1)
 green= Color(0x228b22, 1)
 black= Color(0X000000, 1)
+outline= LineStyle(1, black)
+nooutline= LineStyle(0, red)
 
 class thechoice:
     
