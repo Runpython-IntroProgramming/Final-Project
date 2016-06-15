46138c152f187184074b8739c79fb9801b4f3b64
Suhan Gui <suhangui@hanovernorwichschools.org>
2016-06-14 22:36:05 -0400

    Updated from Brython Server: 6/14/2016 10:35:52 PM

--- experimental.py
+++ experimental.py
@@ -1,23 +1,18 @@
 from ggame import App, Color, Sprite, RectangleAsset, LineStyle, TextAsset, MouseEvent, CircleAsset
 
+white= Color(0xffffff, 1)
 red= Color(0xff0000, 1)
 green= Color(0x228b22, 1)
 black= Color(0X000000, 1)
 outline= LineStyle(1, black)
 nooutline= LineStyle(0, red)
 
-makepix= []
-
-def create():
-    for celle in makepix[:]:
-        Cell((celle[0], celle[1]))
-        makepix.remove(celle)
-
 class thechoice(Sprite):
     
     strings = {'single': 'Single Player',
         'multi': '2 Players',
         }
+    background= RectangleAsset(100,200,
     
     def __init__(self, position):
         super().__init__(thechoice.dia, position)
