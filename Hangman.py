e7e85a90ea1d50b19bc5e0ec68227195658118c2
peterbynum <peterbynum@hanovernorwichschools.org>
2016-10-24 14:00:36 -0400

    Updated from Brython Server: 10/24/2016 2:00:36 PM

--- Hangman.py
+++ Hangman.py
@@ -1,1 +1,26 @@
+"""
+Creator: Peter Bynum
+Sources:
 
+"""
+
+from random import randint
+from ggame import App, Color, LineStyle, Sprite, CircleAsset, RectangleAsset, EllipseAsset, PolygonAsset, TextAsset
+
+SCREEN_WIDTH = 500
+SCREEN_HEIGHT = 300
+
+difficulty = input("Please choose a difficulty level: easy, medium, or hard")
+
+easywords = ['monkey', 'breeze', 'cookies', 'vapor', 'shallow']
+mediumwords = [
+hardwords = [
+
+class Hangman(App):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+
+
+
+myapp = Hangman(SCREEN_WIDTH, SCREEN_HEIGHT)
+myapp.run()
