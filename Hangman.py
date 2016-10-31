9e4b4d7906c8cbe0fbb525d709332bebec3c58a9
peterbynum <peterbynum@hanovernorwichschools.org>
2016-10-31 13:47:24 -0400

    Updated from Brython Server: 10/31/2016 1:47:24 PM

--- Hangman.py
+++ Hangman.py
@@ -15,19 +15,24 @@ easywords = ['monkey', 'toddler', 'cookies', 'image', 'shallow']
 mediumwords = ['bandwagon', 'youthful', 'vaporize', 'pajama', 'whiskey']
 hardwords = ['jazzy', 'abruptly', 'larynx', 'zephyr', 'rhubarb']
 
-class Gallows(Sprite):
-    asset = ImageAsset("gallows.png",
+gallowsasset = ImageAsset("gallows.png",
         Frame(0,0,300,300), 7, 'vertical')
+
+#letterbuttonasset
+#wordbuttonasset
+
+class Gallows(Sprite):
+
     def __init__(self, width, height):
         super().__init__(Gallows.asset, width, height)
         
         
 class Hangman(App):
-    asset = ImageAsset("gallows.png",
-        Frame(0,0,300,300), 7, 'vertical')
     def __init__(self, width, height):
         super().__init__(width, height)
-        Sprite(Gallows(0,0))
+        self.gallows(gallowsasset = ImageAsset("gallows.png",
+            Frame(0,0,300,300), 7, 'vertical')
+
 
 
 myapp = Hangman(SCREEN_WIDTH, SCREEN_HEIGHT)
