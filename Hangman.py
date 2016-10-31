22fff15393b79906bb2a43ce7fc47de06e2d3efc
peterbynum <peterbynum@hanovernorwichschools.org>
2016-10-31 13:33:54 -0400

    Updated from Brython Server: 10/31/2016 1:33:52 PM

--- Hangman.py
+++ Hangman.py
@@ -5,7 +5,7 @@ Sources:
 """
 
 from random import randint
-from ggame import App, Color, LineStyle, Sprite, CircleAsset, RectangleAsset, EllipseAsset, PolygonAsset, TextAsset
+from ggame import App, Color, Frame, ImageAsset, LineStyle, Sprite, CircleAsset, RectangleAsset, EllipseAsset, PolygonAsset, TextAsset
 
 SCREEN_WIDTH = 500
 SCREEN_HEIGHT = 300
@@ -15,15 +15,20 @@ easywords = ['monkey', 'toddler', 'cookies', 'image', 'shallow']
 mediumwords = ['bandwagon', 'youthful', 'vaporize', 'pajama', 'whiskey']
 hardwords = ['jazzy', 'abruptly', 'larynx', 'zephyr', 'rhubarb']
 
-
+class Gallows(Sprite):
+    asset = ImageAsset("gallows.png",
+        Frame(0,0,300,300), 7, 'vertical')
+    def __init__(self, width, height):
+        super().__init__(Gallows.asset, width, height)
+        
+        
 class Hangman(App):
     asset = ImageAsset("gallows.png",
         Frame(0,0,300,300), 7, 'vertical')
     def __init__(self, width, height):
         super().__init__(width, height)
+        Sprite(Gallows(0,0))
 
 
-"""
 myapp = Hangman(SCREEN_WIDTH, SCREEN_HEIGHT)
 myapp.run()
-"""
