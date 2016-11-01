c175289e06dcaf9ab73b0230f64e5efd0535dc7a
peterbynum <peterbynum@hanovernorwichschools.org>
2016-11-01 14:00:28 -0400

    Updated from Brython Server: 11/1/2016 2:00:28 PM

--- Hangman.py
+++ Hangman.py
@@ -9,30 +9,73 @@ from ggame import App, Color, Frame, ImageAsset, LineStyle, Sprite, CircleAsset,
 
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 1000
-#difficulty = input("Please choose a difficulty level: easy, medium, or hard")
 
-easywords = ['monkey', 'toddler', 'cookies', 'image', 'shallow']
-mediumwords = ['bandwagon', 'youthful', 'vaporize', 'pajama', 'whiskey']
-hardwords = ['jazzy', 'abruptly', 'larynx', 'zephyr', 'rhubarb']
-
+blue = Color(0xcce6ff, 1.0)
+noline = LineStyle(0,blue)
 gallowsasset = ImageAsset("gallows.png",
         Frame(0,0,300,300), 7, 'vertical')
+guessletterasset = ImageAsset("guessletterasset.png")
+wordbuttonasset = ImageAsset("guesswordasset.png")
 
-#letterbuttonasset
-#wordbuttonasset
+difficulty = input("Please choose a difficulty level: easy, medium, or hard")
+
+easy = ['monkey', 'toddler', 'cookies', 'image', 'shallow']
+medium = ['bandwagon', 'youthful', 'vaporize', 'pajama', 'whiskey']
+hard = ['jazzy', 'abruptly', 'larynx', 'zephyr', 'rhubarb']
+
+if difficulty=='easy':
+    word = easy[randint(0,4)]
+if difficulty=='medium':
+    word = medium[randint(0,4)]
+if difficulty=='hard':
+    word = hard[randint(0,4)]
+
+
+wrongletters = []
+
+wordasset = TextAsset("hello", '20px Arial')
 
 class Hangman(App):
-    bgasset = RectangleAsset(width, height, noline, 
     def __init__(self, width, height):
         super().__init__(width, height)
-        blaue = Color(0, 1)
-        noline = LineStyle(0, black)
-        self.gallows = Sprite(gallowsasset,(300,0))
+        #creating sprites
+        bgasset = RectangleAsset(SCREEN_WIDTH, 700, noline, blue)
+        Sprite(bgasset,(0,0))
+        self.gallows = Sprite(gallowsasset,(350,50))
         self.gallows.scale = 1.5
+        self.letterbutton = Sprite(guessletterasset,(800,250))
+        self.wordbutton = Sprite(wordbuttonasset,(800,300))
+        self.gallows.hangingphase = 0
+        self.wordsprite = Sprite(wordasset(0,0))
+        
+        
+    def mousemove(self, event):
+        global mousex
+        global mousey
+        mousex = event.x-10
+        mousey = event.y-10
+    
+    def guessletter():
+        print('guessingletter')
+    
+    def guessword():
+        print('guessingword')
+    
+    def mousedown(self, event):
+        global mousex
+        global mousey
+        if (mousex >= 800 and mousex <= 840) and (mousey >= 250 and mousey <= 275):
+            Hangman.guessletter()
+        
+        if (mousex >= 800 and mousex <= 840) and (mousey >= 300 and mousey <= 325):
+            Hangman.guessword()
 
+            
 
 
 
 
 myapp = Hangman(SCREEN_WIDTH, SCREEN_HEIGHT)
+myapp.listenMouseEvent('mousemove', myapp.mousemove)
+myapp.listenMouseEvent('mousedown', myapp.mousedown)
 myapp.run()
