c2973a34c6f9a3dcb3bb5b628e19bb8ba64e8746
T045T3R <johannestestorf@hanovernorwichschools.org>
2016-11-09 13:07:38 -0500

    Updated from Brython Server: 11/9/2016 1:07:40 PM

--- raycast.py
+++ raycast.py
@@ -2,8 +2,10 @@ from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Fra
 
 mapw= 100
 maph=100
-SCREEN_WIDTH = mapw
-SCREEN_HEIGHT = maph
+scrw=640
+scrh=480
+SCREEN_WIDTH = scrw
+SCREEN_HEIGHT = scrh
 
 black = Color(0x000000, 1.0)
 red = Color(0xff0000, 1.0)
@@ -15,10 +17,10 @@ print(width)
 print(height)
 click = 0
 
-colors={"0101":"0"}
+walls={"0101":"0"}
 for x in width:
     for y in height:
-        colors['0'+str(x)+'0'+str(y)]=0
+        walls['0'+str(x)+'0'+str(y)]=0
 
 thinline = LineStyle(1, black)
 rsquare_asset =RectangleAsset(10,10, thinline, red)
@@ -32,12 +34,12 @@ def mouseclick(event):
     click = 1
     pixelpositionx = ((event.x)//10)*10
     pixelpositiony = ((event.y)//10)*10
-    if colors['0'+str(round(pixelpositionx/10))+'0'+str(round(pixelpositiony/10))]==0:
+    if walls['0'+str(round(pixelpositionx/10))+'0'+str(round(pixelpositiony/10))]==0:
         Sprite(rsquare_asset, (pixelpositionx, pixelpositiony))
-        colors['0'+str(round(pixelpositionx/10))+'0'+str(round(pixelpositiony/10))]=1
+        walls['0'+str(round(pixelpositionx/10))+'0'+str(round(pixelpositiony/10))]=1
     else:
         Sprite(wsquare_asset, (pixelpositionx, pixelpositiony))
-        colors['0'+str(round(pixelpositionx/10))+'0'+str(round(pixelpositiony/10))]=0
+        walls['0'+str(round(pixelpositionx/10))+'0'+str(round(pixelpositiony/10))]=0
         
 def mouseup(event):        
     global click
@@ -48,17 +50,18 @@ def drag(event):
     if click==1:
         pixelpositionx = ((event.x)//10)*10
         pixelpositiony = ((event.y)//10)*10
-        if colors['0'+str(round(pixelpositionx/10))+'0'+str(round(pixelpositiony/10))]==0:
+        if walls['0'+str(round(pixelpositionx/10))+'0'+str(round(pixelpositiony/10))]==0:
             Sprite(rsquare_asset, (pixelpositionx, pixelpositiony))
-            colors['0'+str(round(pixelpositionx/10))+'0'+str(round(pixelpositiony/10))]=1
+            walls['0'+str(round(pixelpositionx/10))+'0'+str(round(pixelpositiony/10))]=1
         else:
             Sprite(wsquare_asset, (pixelpositionx, pixelpositiony))
-            colors['0'+str(round(pixelpositionx/10))+'0'+str(round(pixelpositiony/10))]=0
+            walls['0'+str(round(pixelpositionx/10))+'0'+str(round(pixelpositiony/10))]=0
 
 myapp = App(SCREEN_WIDTH, SCREEN_HEIGHT)
 myapp.listenMouseEvent('mouseup', mouseup)
 myapp.listenMouseEvent('mousedown', mouseclick)
 myapp.listenMouseEvent('mousemove', drag)
+myapp.listenKeyEvent('keydown', 'space', spacekey)
 
 myapp = App()
 myapp.run()
