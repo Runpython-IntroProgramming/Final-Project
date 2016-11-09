0d945786e97a10c594cddd0bdb20e953f49bf3fd
liama482 <liamabbate@hanovernorwichschools.org>
2016-11-09 13:15:18 -0500

    Updated from Brython Server: 11/9/2016 1:15:18 PM

--- Final.py
+++ Final.py
@@ -1,1 +1,24 @@
+"""
+by Liam A.
+used: http://www.december.com/html/spec/color
 
+"""
+
+from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
+
+# Colors
+Lgreen = Color (0x7CFC00, 0.95)
+turqo = Color (0x40E0D0, 0.99)
+Orange = Color (0xFF8600, 1)
+black = Color (0x000000, 0.85)
+purp = Color (0x68228B, 0.6)
+brn = Color (0x5C3317, 0.9)
+pale = Color (0xFFFACD, 0.4)
+
+bl_line = LineStyle(3, black)
+thinline = LineStyle(1, black)
+
+Input:("What")
+
+my_draw = App()
+my_draw.run()
