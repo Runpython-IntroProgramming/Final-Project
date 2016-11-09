777060658aba86094166b2b0325c2c2addc0cfaa
T045T3R <johannestestorf@hanovernorwichschools.org>
2016-11-09 13:13:52 -0500

    Updated from Brython Server: 11/9/2016 1:13:55 PM

--- raycast.py
+++ raycast.py
@@ -11,22 +11,25 @@ black = Color(0x000000, 1.0)
 red = Color(0xff0000, 1.0)
 white = Color(0xffffff, 1.0)
 
-width=list(range(0,int((mapw/10))+1))
-height = list(range(0,int((maph/10))+1))
+width=list(range(0,int((scrw/10))+1))
+height = list(range(0,int((scrh/10))+1))
+
+mwidth=list(range(0,int((mapw/10))+1))
+mheight = list(range(0,int((maph/10))+1))
 print(width)
 print(height)
 click = 0
 
 walls={"0101":"0"}
-for x in width:
-    for y in height:
+for x in mwidth:
+    for y in mheight:
         walls['0'+str(x)+'0'+str(y)]=0
 
 thinline = LineStyle(1, black)
 rsquare_asset =RectangleAsset(10,10, thinline, red)
 wsquare_asset = RectangleAsset(10, 10, thinline, white)
-for x in width:
-    for y in height:
+for x in mwidth:
+    for y in mheight:
         Sprite(wsquare_asset,(x*10, y*10))
         
 thinline = LineStyle(1, black)
@@ -68,7 +71,6 @@ myapp = App(SCREEN_WIDTH, SCREEN_HEIGHT)
 myapp.listenMouseEvent('mouseup', mouseup)
 myapp.listenMouseEvent('mousedown', mouseclick)
 myapp.listenMouseEvent('mousemove', drag)
-myapp.listenKeyEvent('keydown', 'space', spacekey)
 
 myapp = App()
 myapp.run()
