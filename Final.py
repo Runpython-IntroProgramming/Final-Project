48be960ddebdd6db29b5e827d46add4547b62655
liama482 <liamabbate@hanovernorwichschools.org>
2016-11-15 13:58:29 -0500

    Updated from Brython Server: 11/15/2016 1:58:29 PM

--- Final.py
+++ Final.py
@@ -45,10 +45,10 @@ class Draw(App):
         super().__init__(width, height)
         Draw.listenMouseEvent("mouseup", self.ym_up)
         Draw.listenMouseEvent("mousedown", self.nm_dn)
-        Bunny(45, 280)
-        Cat(30, 220)
-        Tree(45, 160)
-        Flowr(30, 100)
+        Bunny((45, 280))
+        Cat((30, 220))
+        Tree((45, 160))
+        Flowr((30, 100))
 
     def ym_up(self,event):
         self.a="yes"
