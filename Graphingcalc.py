d13c555f06b6604d597b5a07382861aa2b66da94
Aqkotz <andy.lyme@mac.com>
2016-12-02 13:26:13 -0500

    Updated from Brython Server: 12/2/2016 1:26:13 PM

--- Graphingcalc.py
+++ Graphingcalc.py
@@ -1,27 +1,34 @@
 # Andy Kotz final project: graphing calculator with regressions
 
-from ggame import App, Color, LineStyle, Sprite
+from ggame import App, Color, LineStyle, Sprite, RectangleAsset
 from ggame import CircleAsset
 from math import sin, cos, radians
+
+SCREEN_WIDTH = 1700
+SCREEN_HEIGHT = 1000
+
+m = int(input("m: "))
+b = int(input("b: "))
 
 red = Color(0xff0000, 1.0)
 green = Color(0x00ff00, 1.0)
 blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
 purple = Color(0x9B30FF, 1.0)
-
+thinline = LineStyle(0, black)
+yaxis = RectangleAsset(1, 1000, thinline, black)
+xaxis = RectangleAsset(1700, 1, thinline, black)
 thinline = LineStyle(0.5, black)
-mycircle = CircleAsset(5, thinline, blue)
-mycircle2 = CircleAsset(5, thinline, red)
-mycircle3 = CircleAsset(5, thinline, purple)
-xcoordinates = range(100, 600, 1)
-xcoordinates1 = range(100, 600, 1)
-xcoordinates2 = range(100, 600, 1)
+mycircle = CircleAsset(1, thinline, blue)
+Sprite (xaxis, (0, 500))
+Sprite (yaxis, (850, 0))
 
-# Generate a list of sprites that form a line!
-sprites = [Sprite(mycircle, (100+100*cos(radians(x)), 100+100*sin(radians(x)) + 400)) for x in xcoordinates]
-sprites = [Sprite(mycircle2, (x, 100+100*cos(radians(x)) + 100)) for x in xcoordinates1]
-sprites = [Sprite(mycircle3, (x, 400+100*sin(radians(x)) + 100)) for x in xcoordinates2]
+xcoordinates = range(-1000, 1000, 2)
 
-myapp = App()
+
+#sprites = [Sprite(mycircle, (100+100*cos(radians(x)), 100+100*sin(radians(x)) + 400)) for x in xcoordinates]
+#sprites = [Sprite(mycircle, (x, 100+100*cos(radians(x)) + 100)) for x in xcoordinates]
+sprites = [Sprite(mycircle, (x+850, m*x+b+500)) for x in xcoordinates]
+
+myapp = App(SCREEN_WIDTH, SCREEN_HEIGHT)
 myapp.run()
