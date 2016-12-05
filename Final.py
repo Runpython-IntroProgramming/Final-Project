c3b742354faa12a3b51fc5fa623470e6911af04b
liama482 <liamabbate@hanovernorwichschools.org>
2016-12-05 13:33:49 -0500

    Updated from Brython Server: 12/5/2016 1:33:49 PM

--- Final.py
+++ Final.py
@@ -18,24 +18,38 @@ black = Color (0x000000, 0.85)
 purp = Color (0x68228B, 0.6)
 brn = Color (0x5C3317, 0.9)
 pale = Color (0xFFFACD, 0.4)
+white = Color (0xFFFFFF, 0)
 
 bl_line = LineStyle(3, black)
 thinline = LineStyle(1, black)
+noline = LineStyle(0, white)
+
 clkun=[]
 clkdx=[]
+stage=1
+color=0
+dotg = CircleAsset(1, noline, Lgreen)
+dotq = CircleAsset(1, noline, turqo)
+doto = CircleAsset(1, noline, orange)
+dotb = CircleAsset(1, noline, black)
+dotp = CircleAsset(1, noline, purp)
+dotr = CircleAsset(1, noline, brn)
+dotl = CircleAsset(1, noline, pale)
+
 
 class Icon(Sprite):
     def __init__(self,asset,position,prop):
         self.a="no"
         self.b=0
         self.c=0
+        global stage
         self.both=False
         chk = 0 #preparing to check a condition
         self.ct = 1 #nothing has been clicked on
         super().__init__(asset, position)
         self.center=(0.5,0.5)
         #Draw.listenMouseEvent("mouseup", self.nm_up)
-        if prop==True and Draw.stage==1:
+        if prop==True and stage==1:
             Draw.listenMouseEvent("mousedown", self.ym_dn)
 
     def ym_dn(self,event):
@@ -68,7 +82,7 @@ class Icon(Sprite):
                 lgthb = len(clkdx)
                 clkun[lgtha-1](clkdx[lgthb-1], False) #place the selected icon: @ lgth+2, @ clicked location: lgth+1
                 #print(clkun[lgtha-1], end=' ')
-                #print("!!!THEN!!!", clkdx[lgthb-1])
+                #print(clkdx[lgthb-1])
             
         self.ct += 1
     def nm_up(self,event):
@@ -96,19 +110,41 @@ class Bunny(Icon):
         self.scale = 0.8
 
 class Draw(App):
+    global stage
+    global color
     def __init__(self, width, height):
         super().__init__(width, height)
-        self.stage = 1
+        stage = 1
         abun = Bunny((65, 520), True)
         acat = Cat((85, 380), True)
         atree = Tree((75, 200), True)
         aflr = Flowr((50, 105), True)
         #bflr = Flowr((650, 420))
-        Draw.listenKeyEvent("enter", self.switch)
+        Draw.listenKeyEvent("keydown", "enter", self.switch)
+        Draw.listenKeyEvent("keydown", "g", self.green)
+        Draw.listenKeyEvent("keydown", "q", self.turq)
+        Draw.listenKeyEvent("keydown", "o", self.orange)
+        Draw.listenKeyEvent("keydown", "b", self.black)
+        Draw.listenKeyEvent("keydown", "p", self.purp)
+        Draw.listenKeyEvent("keydown", "r", self.brn)
+        Draw.listenKeyEvent("keydown", "l", self.pale)
+    def switch(self,event):
+        stage += 1
+    def green(self,event):
+        color = 1
+    def turq(self,event):
+        color = 2
+    def orange(self,event):
+        color = 3
+    def black(self,event):
+        color = 4
+    def purp(self,event):
+        color = 5
+    def brn(self,event):
+        color = 6
+    def brn(self,event):
+        color = 7
     def step(self):
-        pass
-    def switch:
-        self.stage += 1
 
 my_draw = Draw(SCREEN_WIDTH, SCREEN_HEIGHT)
 my_draw.run()
