152e4b5e5f337d0cb8b3c3d931ac703ac4c0a055
Aqkotz <andy.lyme@mac.com>
2016-12-05 14:00:48 -0500

    Updated from Brython Server: 12/5/2016 2:00:47 PM

--- Graphingcalc.py
+++ Graphingcalc.py
@@ -39,10 +39,10 @@ for linetype in linetypelist:
         b = float(input("cubic b: "))
         c = float(input("cubic c: "))
         d = float(input("cubic d: "))
-    xcoordinates2 = range(-6000, 6000, 1)
+    xcoordinates2 = range(-3000, 3000, 1)
     xcoordinates = []
     for x in xcoordinates2:
-        x = x/128
+        x = x/64
         xcoordinates.append(x)
 
     if linetype == "l":
