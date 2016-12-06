b17870453e4d73bb6339b69c280870dbe135dabd
Aqkotz <andy.lyme@mac.com>
2016-12-06 14:00:30 -0500

    Updated from Brython Server: 12/6/2016 2:00:23 PM

--- Graphingcalc.py
+++ Graphingcalc.py
@@ -12,9 +12,20 @@ green = Color(0x00ff00, 1.0)
 blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
 purple = Color(0x9B30FF, 1.0)
+grey = Color(0xd3d3d3, 0.7)
 thinline = LineStyle(0, black)
 yaxis = RectangleAsset(1, 1000, thinline, black)
 xaxis = RectangleAsset(1900, 1, thinline, black)
+ycursor =  RectangleAsset(1, 1000, thinline, grey)
+xcursor = RectangleAsset(1900, 1, thinline, grey)
+class Xcursorclass(Sprite):
+    def __init__(self, position):
+        super().__init__(xcursor, position)
+class Ycursorclass(Sprite):
+    def __init__(self, position):
+        super().__init__(ycursor, position)
+xcurse = Xcursorclass((0,0))
+ycurse = Ycursorclass((0,0))
 xaxisrulings = RectangleAsset(1, 7, thinline, black)
 yaxisrulings = RectangleAsset(7, 1, thinline, black)
 thinline = LineStyle(0, black)
@@ -53,7 +64,12 @@ for linetype in linetypelist:
     if linetype == "c":
         sprites = [Sprite(mycircle, (20*x+950, 20*(-a*x**3-b*x**2-c*x-d)+500)) for x in xcoordinates]
 
+def mousePosition(event):
+    xcurse.y = event.y-7
+    ycurse.x = event.x-9
 
 
 myapp = App(SCREEN_WIDTH, SCREEN_HEIGHT)
 myapp.run()
+myapp.listenMouseEvent('mousemove', mousePosition)
+
