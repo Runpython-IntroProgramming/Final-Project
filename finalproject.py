4318b4b13896671a3e5097a4f4958f5b2cf9aae0
marcusjdh <marcushelble@hanovernorwichschools.org>
2016-12-07 13:02:17 -0500

    Updated from Brython Server: December 7, 2016 1:02:17 PM EST

--- finalproject.py
+++ finalproject.py
@@ -1,17 +1,22 @@
 import ggame
 from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
 from random import random
+SCREEN_WIDTH = 1500
+SCREEN_HEIGHT = 910
 red = Color(0xff0000, 1.0)
 green = Color(0x00ff00, 1.0)
 blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
 white = Color(0xffffff, 1.0)
 
-class ball(Sprite):
+class Ball(Sprite):
     asset=ImageAsset("ball.png")
+    def __init__(self, position):
+        super().__init__(Ball.asset, position)
+        Ball((100,100))
     
 
 
-myapp = App()
+myapp = App(SCREEN_WIDTH, SCREEN_HEIGHT)
 myapp.run()
     
