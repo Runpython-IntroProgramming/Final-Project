ced501fb35f2aa618e9720e0c34fccc667a86c7c
marcusjdh <marcushelble@hanovernorwichschools.org>
2016-12-07 12:43:42 -0500

    Updated from Brython Server: December 7, 2016 12:43:42 PM EST

--- finalproject.py
+++ finalproject.py
@@ -1,3 +1,8 @@
 import ggame
 from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
 from random import random
+red = Color(0xff0000, 1.0)
+green = Color(0x00ff00, 1.0)
+blue = Color(0x0000ff, 1.0)
+black = Color(0x000000, 1.0)
+white = Color(0xffffff, 1.0)
