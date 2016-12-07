db4b94a7ec076e16abc03f4e859d3dc22895dd71
marcusjdh <marcushelble@hanovernorwichschools.org>
2016-12-07 12:52:26 -0500

    Updated from Brython Server: December 7, 2016 12:52:26 PM EST

--- finalproject.py
+++ finalproject.py
@@ -6,3 +6,12 @@ green = Color(0x00ff00, 1.0)
 blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
 white = Color(0xffffff, 1.0)
+
+class ball(Sprite):
+    asset=ImageAsset("ball.png")
+    
+
+
+myapp = App()
+myapp.run()
+    
