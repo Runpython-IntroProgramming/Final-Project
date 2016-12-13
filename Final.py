3996eeec527a5adcdb7628237777c1d7816a7a34
liama482 <liamabbate@hanovernorwichschools.org>
2016-12-13 13:34:53 -0500

    Updated from Brython Server: 12/13/2016 1:34:53 PM

--- Final.py
+++ Final.py
@@ -12,10 +12,10 @@ SCREEN_HEIGHT = 1000
 
 # Colors
 Lgreen = Color (0x7CFC00, 0.95)
-turqo = Color (0x40E0D0, 0.99)
+turqo = Color (0x40E0D0, 0.97)
 orange = Color (0xFF8600, 1)
 black = Color (0x000000, 0.85)
-purp = Color (0x68228B, 0.6)
+purp = Color (0x68228B, 0.7)
 brn = Color (0x5C3317, 0.9)
 pale = Color (0xFFFACD, 0.8)
 white = Color (0xFFFFFF, 0)
