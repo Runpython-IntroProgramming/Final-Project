9a57c3ea3f481ed8484986bf7611528e782bb8c3
T045T3R <johannestestorf@hanovernorwichschools.org>
2016-12-20 13:53:00 -0500

    Updated from Brython Server: 12/20/2016 1:53:07 PM

--- raycast.py
+++ raycast.py
@@ -180,7 +180,8 @@ def ray():
             print(walls['0'+str(round(rayx))+'0'+str(round(rayy))])
             if walls['0'+str(round(rayx))+'0'+str(round(rayy))]==1:
                 inpov=abs(raydir-int(Position['dir']))
-               
+                if tile=='0'+str(round(rayx))+'0'+str(round(rayy)):
+                    tiledir=inpov
                 tile='0'+str(round(rayx))+'0'+str(round(rayy))
                 print('wall is detected')
                 xdif=(int(Position['x'])-round(rayx))
@@ -189,9 +190,7 @@ def ray():
                 print(ydif)
                 distance1=(abs(xdif)**2+abs(ydif))**(1/2)
                 print(inpov)
-                distance=distance1*(sin(radians(90-tiledir)))
-                if distance<distance1*(sin(radians(90-inpov))):
-                    tiledir=inpov
+                distance=distance1*(sin(radians(90-tiledir))
                 print(distance) 
                 wallbox=RectangleAsset(((10/float(distance))),10*(200/(float(distance))), thinline, b5)
                 Sprite(wallbox,(screenside+110,(scrh/2)-(10*(200/(float(distance))))/2))
