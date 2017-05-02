db47990afc7ddd0e6ba12347e692086b97997172
JadedCoder712 <thomaspostans@hanovernorwichschools.org>
2017-05-02 14:11:17 -0400

    Create game.py

---
+++ game.py
@@ -0,0 +1,85 @@
+from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
+import math
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 500
+
+class Field(Sprite):
+    field=ImageAsset("images/field.jpg")
+
+    def __init__(self, position):
+         super().__init__(Field.field, position)
+         self.vx=1
+         self.vy=1
+         self.vr=0
+"""
+class AOA:
+    def __init__(self, x, y, r):
+        self.x = x
+        self.y = y
+        self.rotation = r
+    def angle(self):
+"""
+    
+class Plane(Sprite):
+    airplane = ImageAsset("images/28293b2fe5801e03f1f70ed61c8397f6_airplane-clipart-transparent-airplane-clipart-transparent-background_2400-1009.png")
+    def __init__(self, position):
+        super().__init__(Plane.airplane, position)
+        self.vx = 0
+        self.vy = 0
+        self.vr = 0
+        self.scale = 0.1
+        Game.listenKeyEvent("keydown", "d", self.Forward)
+        Game.listenKeyEvent("keydown", "a", self.Slow)
+        Game.listenKeyEvent("keydown", "left arrow", self.Up)
+        Game.listenKeyEvent("keydown", "right arrow", self.Down)
+        Game.listenKeyEvent("keyup", "right arrow", self.Stop)
+        Game.listenKeyEvent("keyup", "left arrow", self.Stop)
+        Game.listenKeyEvent("keydown", "e", self.Print)
+        self.fxcenter = self.fycenter = 0.5
+        
+    def step(self):
+        #while self.vx >= 0:
+        self.x += self.vx
+        #self.x -= 0.4
+        #while self.vy >= 0:
+        self.y += self.vy
+        #self.y -= 0.4
+        self.rotation += self.vr
+        """
+         if self.rotation > 90:
+             self.vx = 0
+             self.y -= 
+        """
+    def Forward(self, event):
+        self.vx += 0.7
+    def Slow(self, event):
+        self.vx -= 0.6
+    def Up(self, event):
+        self.vr += 0.1
+        #self.vy += 0.6
+    def Down(self, event):
+        self.vr -= 0.1
+        #self.vy -= 0.6
+    def Stop(self, event):
+        self.vr=0
+    def Print(self, event):
+        print(self.rotation)
+    
+class Game(App):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        """
+        black = Color(0, 1)
+        noline = LineStyle(0, black)
+        bg_asset = RectangleAsset(SCREEN_WIDTH, SCREEN_HEIGHT, noline, black)
+        bg = Sprite(bg_asset, (0,0))
+        """
+        Field((0,0))
+        Plane((0,500))
+        
+    def step(self):
+        for airplane in self.getSpritesbyClass(Plane):
+            airplane.step()
+            
+myapp = Game(SCREEN_WIDTH, SCREEN_HEIGHT)
+myapp.run()
