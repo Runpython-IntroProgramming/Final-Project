f309f5021fee9ae812146c120091b80855eeab33
afeyrer <abigailfeyrer@hanovernorwichschools.org>
2017-05-03 13:40:51 -0400

    Updated from Brython Server: 5/3/2017 1:40:51 PM

--- snake.py
+++ snake.py
@@ -16,13 +16,9 @@ rond=lambda x: 2*(round(x/2,-1))
 length=1
 x=20
 y=20
-go=1
 snk=[(20,20)]
 
-dot=[(20*randint(0,39), 20*randint(0,29))]
-for (h,k) in dot:
-    tail((h,k))
-    
+
 class tail(Sprite):
     asset=RectangleAsset(20,20,line, purple)
     def __init__(self, position):
@@ -32,82 +28,93 @@ class ntail(Sprite):
     def __init__(self, position):
         super().__init__(ntail.asset, position)
 
+dot=[(20*randint(0,39), 20*randint(0,29))]
+for (h,k) in dot:
+    tail((h,k))
+    
 tail((x,y))
 
-if go==1:
-    def leftKey(event):
-        global up, go, x, y, snk
-        up=0
-        go=1
-        ntail((x,y))
-        x=x-20
-        if (x+20)>SCREEN_WIDTH or (y+20)>SCREEN_HEIGHT or x<0 or y<0:
+def leftKey(event):
+    global x, y, snk, dot
+    x=x-20
+    for (h,k) in dot:
+        if x==h and y==k:
+            snk.append((x,y))
+            dot[0]=(20*randint(0,39), 20*randint(0,29))
+            for (z,q) in dot:
+                tail((z,q))
+        elif (x+20)>SCREEN_WIDTH or (y+20)>SCREEN_HEIGHT or x<0 or y<0:
             print("you lose")
             go=0
+            ntail(snk[0])
         else:
+            ntail(snk[0])
             tail((x,y))
             snk.append((x,y))
             snk.remove(snk[0])
-    def rightKey(event):
-        global up, go, x, y, snk
-        up=0
-        go=1
-        ntail((x,y))
-        x=x+20
-        for (h,k) in dot:
-            if x==h and y==k:
-                
-            elif (x+20)>SCREEN_WIDTH or (y+20)>SCREEN_HEIGHT or x<0 or y<0:
-                print("you lose")
-                go=0
-            
-            else:
-                tail((x,y))
-                snk.append((x,y))
-                snk.remove(snk[0])
 
-    def upKey(event):
-        global up, go, x, y, snk
-        up=0
-        go=1
-        ntail((x,y))
-        y=y-20
-        if (x+20)>SCREEN_WIDTH or (y+20)>SCREEN_HEIGHT or x<0 or y<0:
+def rightKey(event):
+    global x, y, snk, dot
+    x=x+20
+    for (h,k) in dot:
+        if x==h and y==k:
+            snk.append((x,y))
+            dot[0]=(20*randint(0,39), 20*randint(0,29))
+            for (z,q) in dot:
+                tail((z,q))
+        elif (x+20)>SCREEN_WIDTH or (y+20)>SCREEN_HEIGHT or x<0 or y<0:
             print("you lose")
             go=0
+            ntail(snk[0])
         else:
+            ntail(snk[0])
             tail((x,y))
             snk.append((x,y))
             snk.remove(snk[0])
-    def downKey(event):
-        global up, go, x, y, snk
-        up=0
-        go=1
-        ntail((x,y))
-        y=y-20
-        if (x+20)>SCREEN_WIDTH or (y+20)>SCREEN_HEIGHT or x<0 or y<0:
+
+def upKey(event):
+    global x, y, snk
+    y=y-20
+    for (h,k) in dot:
+        if x==h and y==k:
+            snk.append((x,y))
+            dot[0]=(20*randint(0,39), 20*randint(0,29))
+            for (z,q) in dot:
+                tail((z,q))
+        elif (x+20)>SCREEN_WIDTH or (y+20)>SCREEN_HEIGHT or x<0 or y<0:
             print("you lose")
             go=0
+            ntail(snk[0])
         else:
+            ntail(snk[0])
             tail((x,y))
             snk.append((x,y))
             snk.remove(snk[0])
-    """def spaceKey(event):
-        global go
-        go=0"""
 
+def downKey(event):
+    global x, y, snk
+    y=y+20
+    for (h,k) in dot:
+        if x==h and y==k:
+            snk.append((x,y))
+            dot[0]=(20*randint(0,39), 20*randint(0,29))
+            for (z,q) in dot:
+                tail((z,q))
+        elif (x+20)>SCREEN_WIDTH or (y+20)>SCREEN_HEIGHT or x<0 or y<0:
+            print("you lose")
+            go=0
+            ntail(snk[0])
+        else:
+            ntail(snk[0])
+            tail((x,y))
+            snk.append((x,y))
+            snk.remove(snk[0])
+    
 
-
-if (x+20)>SCREEN_WIDTH or (y+20)>SCREEN_HEIGHT or x<0 or y<0:
-        go=0
-        print("You lose.")   
-        
-        
-        
 myapp = App(SCREEN_WIDTH, SCREEN_HEIGHT)
 myapp.run()
 myapp.listenKeyEvent('keydown', 'j', leftKey)
 myapp.listenKeyEvent('keydown', 'i', upKey)
 myapp.listenKeyEvent('keydown', 'k', downKey)
 myapp.listenKeyEvent('keydown', 'l', rightKey)
-#myapp.listenKeyEvent('keydown', 'space', spaceKey)
+#myapp.listenKeyEvent('keyup', 'space', spaceKey)
