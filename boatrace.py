9edbbbcaa4e724220c76e5b4d3bd09b5cc756d74
willl4 <williamlaycock@hanovernorwichschools.org>
2017-05-16 14:55:53 -0400

    Updated from Brython Server: 5/16/2017 2:55:52 PM

--- boatrace.py
+++ boatrace.py
@@ -125,7 +125,7 @@ class Ship(Sprite):
         self.vr = -0.1
         
 class Ship2(Ship):
-    asset = ImageAsset("Images/boat3.jpg")
+    asset = ImageAsset("images/boat3.jpg")
     
     def assignkeys(self):
         BoatGame.listenKeyEvent("keydown", "w", self.thrustOn)
