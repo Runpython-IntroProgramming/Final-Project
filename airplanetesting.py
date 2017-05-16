f626042cd040331edaf97566fc0035ba6b257f7a
JadedCoder712 <thomaspostans@hanovernorwichschools.org>
2017-05-16 15:04:16 -0400

    Updated from Brython Server: 5/16/2017 3:04:15 PM

--- airplanetesting.py
+++ airplanetesting.py
@@ -254,7 +254,7 @@ class Plane(Sprite):
                     self.rotation -= 0.02
                 if (-0.10 < self.rotation < 0.10) is True:
                     self.rotation = 0
-            if (6.283 > self.rotation > 3.14):
+            if (3.14 < self.rotation < 6.283):
                 if (-0.10 < self.rotation < 0.10) is False:
                     self.rotation += 0.02
                 if (-0.10 < self.rotation < 0.10) is True:
