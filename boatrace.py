f22e9dc4b6b501de58c9ad4b5820960c3faa4a06
willl4 <williamlaycock@hanovernorwichschools.org>
2017-05-24 13:25:34 -0400

    Updated from Brython Server: 5/24/2017 1:25:34 PM

--- boatrace.py
+++ boatrace.py
@@ -180,7 +180,7 @@ class BoatGame(App):
     def __init__(self, width, height):
         super().__init__()
         ocean = Ocean((0,0))
-        tal = tally5((self.width-50,self.height-50))
+        tal = tally5((self.width-50, 30))
         ocean.scale = self.width/ocean.width
         self.ss = Ship((300,200))
         self.sv= Ship2((600,200))
