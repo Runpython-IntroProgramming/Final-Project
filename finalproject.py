e8348ce38954dcbe016124ae7aa13cb7502e6452
samuelpych <samuelpych@hanovernorwichschools.org>
2017-05-26 14:29:24 -0400

    Updated from Brython Server: 5/26/2017 2:29:23 PM

--- finalproject.py
+++ finalproject.py
@@ -1,1 +1,21 @@
+"""
+Finalproject.py
+Author: Sam Pych
+Credit:
+Assignment: Create a pong game with two movable blocks and the ball either bounces off the wall 
+or appears on the other side.
+optional: keep score
+"""
+from ggame import App, Sprite, ImageAsset, Frame
+from ggame import SoundAsset, Sound, TextAsset, Color
+import math
+from time import time
 
+from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
+
+Class Pongblock1(Sprite):
+    asset=ImageAsset("",Frame(227,0,292-227,125), 4, 'vertical'))
+    
+
+Class Pongblock2(Sprite):
+    asset=ImageAsset(""Frame(227,0,292-227,125), 4, 'vertical'))
