1bbeaad57e7dea947890ba5599c424470395d4d6
samuelpych <samuelpych@hanovernorwichschools.org>
2017-06-08 14:38:33 -0400

    Updated from Brython Server: 6/8/2017 2:38:33 PM

--- finalproject.py
+++ finalproject.py
@@ -14,27 +14,35 @@ from time import time
 from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
 from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
 
-class Pongblock1(Sprite):
+class Pongblock(Sprite):
     black = Color(0x000000, 1.0)
     thinline= LineStyle(1, black)
-    rectangle=RectangleAsset(400, 100, thinline, black)
+    rectangle_asset=RectangleAsset(50, 200, thinline, black)
+    rectangle = Sprite(rectangle_asset, (60,60))
+    rectangle1 = Sprite(rectangle_asset, (1400,60))
     def __init__(self, position):
-        super().__init__(SpaceShip.asset, position)
         self.vx = 1
         self.vy = 1
         ponggame.listenKeyEvent("keydown", "w", self.up)
         ponggame.listenKeyEvent("keydown","s",self.down)
     def up(self, event):
-        self.vy -=2
+        self.vy -= 2
     def down(self, event):
         self.vy +=2
-
-class Pongblock2(Pongblock1):
+        
+class Pongblock1(Pongblock):
+    print("kyle")
+class Pongblock2(Pongblock):
     print("kyle")
 class pongball(Sprite):
      print("kyle")
 
 class ponggame(App):
-     print("kyle")
+    def __init__(self , width, height):
+        super().__init__(width, height)
+        Pongblock1((10,10))
+        
+        Pongblock2((350,250))
+        
 app=ponggame(0,0)
 app.run()
