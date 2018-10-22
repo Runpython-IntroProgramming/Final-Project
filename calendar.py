4e457e9ef9f61f692d8c1f77cf66fd4ad3de13a7
chenmasterandrew <30731383+chenmasterandrew@users.noreply.github.com>
2018-10-22 12:16:47 -0400

    Updated from Brython Server: 10/22/2018 12:16:47 PM

--- calendar.py
+++ calendar.py
@@ -3,20 +3,22 @@
 # edit "subjects" list as necessary, but keep "other" first
 subjects = ["other", "latin", "geopolitics", "psychology", "chemistry", "computerprogramming", "philosophy", "precalculus", "debate", "modelun", "boyscouts"]
 
-subjectdefaultother = True
+subjectdefaultother = False
 
 
 def rawinput():
     subject = subjectmanager()
     print (subject)
+
 def subjectmanager():
     rawsubject = input("subject: ").lower().replace(" ","")
     for s in subjects:
         if rawsubject == s[0:len(rawsubject)]:
             return s
-
+    if
     if subjectdefaultother:
-        print ('Does not match a subject. Default to "other".')
+        print ('Does not match a subject. Defaulting to "other".')
         return "other"
+    
 rawinput()
     
