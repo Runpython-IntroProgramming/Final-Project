4eaaa22b1c3d67403f7f2bd1b4c3a7b415881367
chenmasterandrew <30731383+chenmasterandrew@users.noreply.github.com>
2018-10-22 09:52:09 -0400

    Update calendar.py

--- calendar.py
+++ calendar.py
@@ -1,6 +1,12 @@
-subjects = ["other", "latin", "geopolitics", "]
+
+
+# edit "subjects" list as necessary, but keep "other" first
+subjects = ["other", "latin", "geopolitics", "psychology", "chemistry", "computerprogramming", "philosophy", "precalculus", "debate", "modelun", "boyscouts"]
 def rawinput():
     subject = subjectmanager()
 
 def subjectmanager():
+    rawsubject = input("subject: ").lower().replace("\\s","")
+    print (rawsubject)
+rawinput()
     
