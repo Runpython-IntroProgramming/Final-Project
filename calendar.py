f314e8f4f00013ae790775bb02fa747dbded159b
chenmasterandrew <30731383+chenmasterandrew@users.noreply.github.com>
2018-10-22 10:29:19 -0400

    Update calendar.py

--- calendar.py
+++ calendar.py
@@ -4,9 +4,13 @@
 subjects = ["other", "latin", "geopolitics", "psychology", "chemistry", "computerprogramming", "philosophy", "precalculus", "debate", "modelun", "boyscouts"]
 def rawinput():
     subject = subjectmanager()
-
+    print (subject)
 def subjectmanager():
-    rawsubject = input("subject: ").lower().replace("\\s","")
-    print (rawsubject)
+    rawsubject = input("subject: ").lower().replace(" ","")
+    for s in subjects:
+        if rawsubject == s[0:len(rawsubject)]:
+            return s
+    print ("Does not match a subject. Try again")
+    subjectmanager()
 rawinput()
     
