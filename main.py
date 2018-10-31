1f7388c6484c589a655b1b570c3822990c58ff9a
chenmasterandrew <30731383+chenmasterandrew@users.noreply.github.com>
2018-10-30 22:05:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
 from utility import subjectManager
-from datetime import datetime
-thisweekday = datetime.today().weekday()
-thistime = str(datetime.today())
-thismonth = int(thistime[5:7])
-thisday = int(thistime[8:10])
-thisyear = int(thistime[2:4])
-leapyears = []
 # x in repeatsuntildefault
 
 #Day config
@@ -16,28 +9,13 @@ nodaydefaulttotomorrow = False
 dayerrordefaulttotomorrow = False
 
 
-"""
-def daysinmonth(month)
-if month in [4, 6, 9, 11]:
-    return 30
-elif thismonth == 2:
-    if
-    else:
-        daysthismonth = 29
-else:
-    return 31
-"""
-#Configuration
 
 
 def newevent():
     rawsubject = input("subject: ")
-    newsubject = subjectManager(rawsubject, 0)
+    newsubject = subjectManager(rawsubject)
     print (newsubject)
+    
 
-"""
-    newtitle = input("title: ")
-    newday = daymanager(1)
-    print (newday)
-"""
+
 newevent()
