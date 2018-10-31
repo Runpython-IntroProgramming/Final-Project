8d0eb297e087e3538f08737a61f44dac7b36f906
chenmasterandrew <30731383+chenmasterandrew@users.noreply.github.com>
2018-10-30 22:01:08 -0400

    Updated from Brython Server: ‎10‎/‎30‎/‎2018 ‎10‎:‎01‎:‎09‎ ‎PM

--- utility.py
+++ utility.py
@@ -1,10 +1,9 @@
-#SUBJECTMANAGER
+###subjectManager###
 #configuration
 subjects = ["other", "latin", "geopolitics", "psychology", "chemistry", "computerprogramming", "philosophy", "precalculus", "debate", "modelun", "boyscouts"]
     #edit "subjects" list as necessary, but keep "other" first
 noSubjectDefaultOther = True
 subjectErrorDefaultOther = True
-
 def subjectManager(rawsubject):
     rawsubject = rawsubject.lower().replace(" ","")
     for s in subjects:
@@ -15,4 +14,30 @@ def subjectManager(rawsubject):
                 return s
         elif subjectErrorDefaultOther:
             return "other"
+###subjectManager###
+
+###calendarManager###
+from datetime import datetime
+thisweekday = datetime.today().weekday()
+thistime = str(datetime.today())
+thismonth = int(thistime[5:7])
+thisday = int(thistime[8:10])
+thisyear = int(thistime[2:4])
+def leapYear(year):
+    if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+        return True
+    else:
+        return False
+def daysInMonth(month, year):
+    if month in [4, 6, 9, 11]:
+        return 30
+    elif thismonth == 2:
+        if leapYear(year):
+            return 29
+        else:
+            return 28
+    else:
+        return 31
+def calendarManager:
+    
 
