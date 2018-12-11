c31313613ddf01740a786f0c2fe6a7269f1d5a3f
ellaedmonds <35499735+ellaedmonds@users.noreply.github.com>
2018-12-11 17:01:12 -0500

    Updated from Brython Server: 12/11/2018 5:01:11 PM

--- project.py
+++ project.py
@@ -39,12 +39,13 @@ class Grid(Sprite):
 
 class Block(Sprite):
     grey = Color(0xC0C0C0, 1.0)
-    white = Color(0xffffff, 1.0)
+    black = Color(0x000000, 1.0)
     side = LineStyle(1,grey)
-    square = RectangleAsset(30,30,side,white)
+    square = RectangleAsset(30,30,side,black)
     def __init__(self,position):
-        super().__init__(Grid.square,position)
+        super().__init__(Block.square,position)
 
+#Block((60,60))
 #Grid((60,60))
 #Sprite(grid,(30,30))
 
@@ -58,6 +59,11 @@ class FireBoy(Sprite):
 
 class Game(App):
     
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        Game.listenMouseEvent("click",self.baby)
+    
+    Cells = []
     def __init__(self):
         
         super().__init__()
@@ -66,10 +72,23 @@ class Game(App):
         for b in range(18):
             for a in range(25):
                 Grid((x,y))
+                #print((x,y))
+                self.Cells.append((x,y))
                 x = x + 30
             x = 0
             Grid((x,y))
+            #print((x,y))
+            self.Cells.append((x,y))
             y = y + 30
+            
+        #print(self.Cells)
+    
+    def baby(self,event):
+        print("hi")
+        for m in self.Cells:
+            if m[0] <= event.x <= m[0]+30:
+                if m[1] <= event.y <= m[1]+30:
+                    Block((m[0],m[1]))
     
 
 
