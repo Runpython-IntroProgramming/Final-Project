856ff9cc1857806c280a67912414781c801eb34e
chenmasterandrew <30731383+chenmasterandrew@users.noreply.github.com>
2018-12-11 20:35:30 -0500

    Update utility.py

--- utility.py
+++ utility.py
@@ -1,47 +1,103 @@
-###subjectManager###
-#configuration
-subjects = ["other", "latin", "geopolitics", "psychology", "chemistry", "computerprogramming", "philosophy", "precalculus", "debate", "modelun", "boyscouts"]
-    #edit "subjects" list as necessary, but keep "other" first
-noSubjectDefaultOther = True
-subjectErrorDefaultOther = True
-def subjectManager(rawsubject):
-    rawsubject = rawsubject.lower().replace(" ","")
-    for s in subjects:
-        if rawsubject == s[0:len(rawsubject)]:
-            if noSubjectDefaultOther:
-                return s
-            elif len(rawsubject) > 0:
-                return s
-        elif subjectErrorDefaultOther:
-            return "other"
-
-###dateManager###
 from datetime import datetime, timedelta
 from calendar import calendar, monthrange
 thisyear = datetime.now().year
-today = datetime.now().date()
-def wordToDate(day, skip):
-    days = []
-    if day == "today":
-        return today
-    elif day == "tomorrow":
-        return today + timedelta(days=1)
-    else:
-        for x in range(7):
-            if day == (today + timedelta(days=x)).weekday():
-                if skip == 5:
-                    return today + timedelta(days=x+7)
-                else:
-                    return today + timedelta(days=x)
-daywords = ["tomorrow", "today", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
-def dateManager(rawdate):
-    rawdate = rawdate.lower().replace("-","/").replace("~","/")
-    skip = 0
-    if rawdate[0:5] == "next ":
-        skip = 5
-    for day in daywords:
-        if rawdate[skip:len(rawdate)] == day[0:len(rawdate) - skip]:
-            return wordToDate(daywords.index(day)-2, skip)
+today = datetime.now().date
+with open("subjectDump.txt", 'r') as d:
+    subjects = [line.rstrip('\n') for line in d]
+
+def saveSubjects():
+    with open('subjectDump.txt', 'w') as d:
+        for s in subjects:
+            d.write(str(s) + '\n')
+
+def addSubject(subject):
+    subject = str(subject)
+    reserved = ["today", "tomorrow", "next", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
+    conflict = False
+    for r in reserved:
+        if subject == r[:len(subject)]:
+            conflict = True
+    for s in subjects:
+        if subject == s[:len(subject)]:
+            conflict = True
+    if not conflict:
+        subjects.append(subject)
+    saveSubjects()
+
+def delSubject(subject):
+    if subject in subjects:
+        subjects.remove(subject)
+    saveSubjects()
+
+days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
+def weekdayManager(rawInput):
+    for d in days:
+        if rawInput == d[:len(rawInput)]:
+            return days.index(d),rawInput[len(d)+1:]
+
+
+
+
+
+
+
+def orderManager(rawInput):
+#tests 11th - 13th
+    if rawInput[:4] == "11th":
+        return 11, rawInput[5:]
+    elif rawInput[:4] == "12th":
+        return 12, rawInput[5:]
+    elif rawInput[:4] == "13th":
+        return 13, rawInput[5:]
+#tests 1st -10th 14th - 366th
+    for num in list(range(1,11)) + list(range(14,367)):
+        if str(num)[-1] == "1":
+            th = str(num) + "st"
+        elif str(num)[-1] == "2":
+            th = str(num) + "nd"
+        elif str(num)[-1] == "3":
+            th = str(num) + "rd"
+        else:
+            th = str(num) + "th"
+        if rawInput[:len(th)] == th:
+            return num, rawInput[len(th)+1:]
+#tests 1 - 366
+    a = [0]
+    for n in range(1,367):
+        if rawInput[:len(str(n))] == str(n):
+            a.append(n)
+    if len(a) > 1:
+        return a[-1],rawInput[len(str(a[-1]))+1:]
+#tests first - threehundredninetyninth
+    teens = ["eleventh", "twelfth", "thirteenth", "fourteenth", "fifteenth", "sixteenth", "seventeenth", "eighteenth", "ninteenth"]
+    ieths = ["tenth", "twentieth", "thirtieth", "fortieth", "fiftieth", "sixtieth", "seventieth", "eightieth", "nintieth"]
+    hundreths = ["one hundreth", "two hundreth", "three hundreth"]
+    ones = ["first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth"]
+    tens = ["twenty-", "thirty-", "forty-", "fifty-", "sixty-", "seventy-", "eighty-", "ninety-"]
+    hundreds = ["", "one hundred ", "two hundred ", "three hundred "]
+    for hundred in hundreds:
+        for teen in teens:
+            num = hundred + teen
+            if num == rawInput[0:len(num)]:
+                return hundreds.index(hundred) * 100 + teens.index(teen) + 11, rawInput[len(num)+1:]
+        for ieth in ieths:
+            num = hundred + ieth
+            if num == rawInput[0:len(num)]:
+                return hundreds.index(hundred) * 100 + (ieths.index(ieth) + 1) * 10, rawInput[len(num)+1:]
+        for ten in tens:
+            for one in ones:
+                num = hundred + ten + one
+                if num == rawInput[0:len(num)]:
+                    return hundreds.index(hundred) * 100 + (tens.index(ten) + 2) * 10 + ones.index(one) + 1, rawInput[len(num)+1:]
+    for hundreth in hundreths:
+        if hundreth == rawInput[0:len(hundreth)]:
+            return (hundreths.index(hundreth) + 1) * 100, rawInput[len(hundreth)+1:]
+    for one in ones:
+        if one == rawInput[0:len(one)]:
+            return ones.index(one) + 1, rawInput[len(one)+1:]
+
+def numericalDateManager(rawInput):
+    rawdate = rawInput.replace("-","/").replace("~","/")
     slashlist = []
     for index in range(len(rawdate)):
         if rawdate[index] == "/":
@@ -50,11 +106,11 @@ def dateManager(rawdate):
     month = int(toslash1)
     if month in range(1,13):
         if len(slashlist) == 1:
-            slash1toend = rawdate[slashlist[0]+1:len(rawdate)]
+            slash1toend = rawdate[slashlist[0]+1:]
             year = thisyear
             day = int(slash1toend)
         elif len(slashlist) == 2:
-            slash2toend = rawdate[slashlist[1]+1:len(rawdate)]
+            slash2toend = rawdate[slashlist[1]+1:]
             slash1toslash2 = rawdate[slashlist[0]+1:slashlist[1]]
             if len(slash2toend) == 0:
                 year = thisyear
@@ -65,22 +121,120 @@ def dateManager(rawdate):
             day = int(slash1toslash2)
         if day in range(1,monthrange(year,month)[1]+1):
             return datetime(year, month, day).date()
-    return
 
-print(dateManager(input("Write a day of the week or date in (mm/dd/yyyy). DateManager will retrieve the numerical date: ")))
+def dynamicDateManager(rawInput):
+    print("test")
+
+def staticDateManager(rawInput, modifier):
+    if rawInput == "tomorrow"[0:len(rawInput)]:
+        return today() + timedelta(days=1)
+    if rawInput == "today"[0:len(rawInput)]:
+        return today()
+#finds "next mon" and "mon"
+    try:
+        for x in range(7):
+            if weekdayManager(rawInput)[0] == (today() + timedelta(days=x)).weekday():
+                if 0 in modifier:
+                    return today() + timedelta(days=x+7)
+                else:
+                    return today() + timedelta(days=x)
+    except:
+        try:
+            numericalDate = numericalDateManager(rawInput)
+            return numericalDate
+            order,rawInput = orderManager(rawInput)
+            print (order)
+            print (rawInput)
+            """
+            interval = weekdayManager(rawInput)[0]
+            rawInput = weekdayManager(rawInput)[1]
+            print (interval)
+            print(rawInput)
+            """
+        except:
+            """
+            interval = monthManager(rawInput)[0]
+            """
+#finds "3rd mon of october" or "5th mon" or "5th mon"
+
+
+
+
+
+
+    """
+    return numericalDateManager(rawInput)
+    """
+def tagManager(rawInput):
+    rawInput = rawInput.lower()
+
+    rawSubject = rawInput.replace(" ","")
+    for s in subjects:
+        if rawSubject == s[0:len(rawSubject)]:
+            return s
+
+    rawInput = rawInput.replace("the ", "").replace("in ","").replace("of ", "")
+    modifiers = ["next ", "until ", "every "]
+    modifier = []
+    for m in modifiers:
+        if rawInput[0:len(m)] == m:
+            modifier.append(modifiers.index(m))
+            rawInput = rawInput[len(m):len(rawInput)]
+    for m in modifiers:
+        if rawInput[0:len(m)] == m:
+            modifier.append(modifiers.index(m))
+            rawInput = rawInput[len(m):len(rawInput)]
+
+    if 2 in modifier:
+        return dynamicDateManager(rawInput)
+    else:
+        return staticDateManager(rawInput, modifier)
+
+
+
+
+
 """
-###calendarManager###
+    if modifier == 0
 
-def calendarManager(year, month):
-    for day in range(1,calendar.monthrange(year, month)[1]+1):
-        Sprite(datetime.datetime(year, month, day).weekday()
+    else:
 
 
-print(datetime.datetime(2018, 10, 31, 13))
-print(datetime.datetime.today().weekday())
 
-calendarManager(2018,10)
 
-app = App()
-app.run()
+
+    if modifier == 1:
+        orders = ["1st", "first", "2nd", "second", "3rd", "third", "4th", "fourth", "5th", "fifth"]
+        for o in orders
+
+
+class Recurr():
+    def __init__(self, interval, end)
+class WeekdayRecurr():
+    def __init__(self, interval, end)
+class Recurr():
+    def __init__(self, interval, end)
+class Event():
+    def __init__(self, name, body, tags):
+        self.name() = name
+        self
+
 """
+
+
+
+run = True
+while run:
+    choice = input("Type 'i' for an input, 'a' to add a subject, or 'd' to delete a subject. Type 'e' to exit.")
+    if choice == "i":
+        print(tagManager(input("Write Something: ")))
+    elif choice == "a":
+        print (subjects)
+        addSubject(input("type what you want to add"))
+        print (subjects)
+    elif choice == "d":
+        print (subjects)
+        delSubject(input("type what you want to delete"))
+        print (subjects)
+    elif choice == "e":
+        run = False
