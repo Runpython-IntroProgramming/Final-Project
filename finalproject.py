acfb4e6b1aecd390695279da7e714e5e6fe3e735
raLiinu30 <46948627+raLiinu30@users.noreply.github.com>
2019-04-30 14:27:10 -0400

    Updated from Brython Server: 4/30/2019 2:27:09 PM

--- finalproject.py
+++ finalproject.py
@@ -1,1 +1,12 @@
-# my program
+# Rain's final project
+from math import sin, cos, radians 
+
+
+R = input('Radius of circle 1: ')
+r = input('Radius of circle r: ')
+a = input('Angle: ')
+f = input('Pen position: ')
+
+# point of pen
+xpen = (R-r)*cos(radians(a)) + rf* cos((R/r-1)a)
+ypen = (R-r)*sin(radians(a)) - rf*sin((R/r-1)a)
