e632665420f0a8e07480c19a00604bf02b54e6ad
raLiinu30 <46948627+raLiinu30@users.noreply.github.com>
2019-05-01 13:29:34 -0400

    Updated from Brython Server: 5/1/2019 1:29:33 PM

--- finalproject.py
+++ finalproject.py
@@ -1,12 +1,19 @@
 # Rain's final project
-from math import sin, cos, radians 
+from math import sin, cos, pi
 
 
-R = input('Radius of circle 1: ')
-r = input('Radius of circle r: ')
-a = input('Angle: ')
-f = input('Pen position: ')
+R = float(input('Radius of circle 1: '))
+r = float(input('Radius of circle r: '))
+a = float(input('Angle: '))
+f = float(input('Pen position: '))
+
+# f is how big the spirograph is 
+
+#converts values of a into radians 
+an = a*180/pi
 
 # point of pen
-xpen = (R-r)*cos(radians(a)) + rf* cos((R/r-1)a)
-ypen = (R-r)*sin(radians(a)) - rf*sin((R/r-1)a)
+xpen = (R-r)*cos(a) + r*f*cos(((R/r)-1)*a)
+ypen = (R-r)*sin(a) - r*f*sin(((R/r)-1)*a)
+print(xpen)
+print(ypen)
