cb1d8af2fe026346c861d4ef79814e7f4edac407
raLiinu30 <46948627+raLiinu30@users.noreply.github.com>
2019-05-01 13:59:43 -0400

    Updated from Brython Server: 5/1/2019 1:59:42 PM

--- finalproject.py
+++ finalproject.py
@@ -1,19 +1,29 @@
 # Rain's final project
 from math import sin, cos, pi
+from ggame import App, Color, LineStyle, Sprite, CircleAsset
+
+black = Color(0x000000, 1.0)
+thinline = LineStyle(1, black)
 
 
 R = float(input('Radius of circle 1: '))
 r = float(input('Radius of circle r: '))
-a = float(input('Angle: '))
 f = float(input('Pen position: '))
 
 # f is how big the spirograph is 
 
-#converts values of a into radians 
-an = a*180/pi
+#converts values of a into degrees 
+# make a range from 0 to 150.
 
 # point of pen
-xpen = (R-r)*cos(a) + r*f*cos(((R/r)-1)*a)
-ypen = (R-r)*sin(a) - r*f*sin(((R/r)-1)*a)
-print(xpen)
-print(ypen)
+for a in range (0,150):
+    xpen = (R-r)*cos(a) + r*50*cos(((R/r)-1)*a)+200
+    ypen = (R-r)*sin(a) - r*50*sin(((R/r)-1)*a)+200
+    print(xpen, ypen)
+    spirograph = CircleAsset(5, thinline, black)
+    Sprite(spirograph, (xpen,ypen))
+
+
+
+myapp = App()
+myapp.run()
