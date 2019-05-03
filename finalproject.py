1a700b7b188298bab86bb954835f7944f7c24427
raLiinu30 <46948627+raLiinu30@users.noreply.github.com>
2019-05-03 15:05:54 -0400

    Updated from Brython Server: 5/3/2019 3:05:54 PM

--- finalproject.py
+++ finalproject.py
@@ -5,24 +5,18 @@ from ggame import App, Color, LineStyle, Sprite, CircleAsset
 black = Color(0x000000, 1.0)
 thinline = LineStyle(1, black)
 
-class SpaceShip(Sprite):
-    def __init__(asset, xpen, ypen, app):
-        R = float(input('Radius of circle 1: '))
-        r = float(input('Radius of circle 2: '))
-        xpen = (R-r)*cos(a) + r*50*cos(((R/r)-1)*a)+200
-        ypen = (R-r)*sin(a) - r*50*sin(((R/r)-1)*a)+200
-        asset = CircleAsset(.5, thinline, black)
-        super().__init__(asset, xpen, ypen, app)
+R = float(input('Radius of circle 1: '))
+r = float(input('Radius of circle 2: '))
+
+a=0
+
+def step():
+    global a
+    a=a+0.01
+    xpen = (R-r)*cos(a) + r*50*cos(((R/r)-1)*a)+200
+    ypen = (R-r)*sin(a) - r*50*sin(((R/r)-1)*a)+200
+    asset = CircleAsset(.5, thinline, black)
+    Sprite(asset, (xpen,ypen))
     
-     def step(self):
-        super().step()
-        self.time += 1
-        if self.time % 100 == 0:
-            Bolt(self.direction, 
-                 self.x+self.width//2,
-                 self.y+10,
-                 self.app)
-            self.direction *= -1
-            
 myapp = App()
-myapp.run()
+myapp.run(step)
