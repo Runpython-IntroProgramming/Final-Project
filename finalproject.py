0b477ffc0749a0278bd80b5bbecee9f8f3173bcd
raLiinu30 <46948627+raLiinu30@users.noreply.github.com>
2019-05-06 14:26:04 -0400

    Updated from Brython Server: 5/6/2019 2:26:02 PM

--- finalproject.py
+++ finalproject.py
@@ -1,6 +1,6 @@
 # Rain's final project
 from math import sin, cos, pi
-from ggame import App, Color, LineStyle, Sprite, CircleAsset
+from ggame import App, Color, LineStyle, Sprite, CircleAsset, LineAsset
 
 black = Color(0x000000, 1.0)
 thinline = LineStyle(1, black)
@@ -12,11 +12,13 @@ a=0
 
 def step():
     global a
-    a=a+0.0008
-    xpen = (R-r)*cos(a) + r*50*cos(((R/r)-1)*a)+200
-    ypen = (R-r)*sin(a) - r*50*sin(((R/r)-1)*a)+200
+    a=a+0.005
+    xpen = (R-r)*cos(a) + r*50*cos(((R/r)-1)*a)+250
+    ypen = (R-r)*sin(a) - r*50*sin(((R/r)-1)*a)+240
     asset = CircleAsset(.5, thinline, black)
+    asset2 = LineAsset(30, 30,thinline)
     Sprite(asset, (xpen,ypen))
+    Sprite(asset2, (xpen, ypen))
     
 myapp = App()
 myapp.run(step)
