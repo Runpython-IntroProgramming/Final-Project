78dc888e25409a52d4bcd0ce7284255e5fe38b68
anEndrel23 Hhs <44811950+anEndrel23@users.noreply.github.com>
2019-05-07 22:18:18 -0400

    Updated from Brython Server: 5/7/2019 10:18:18 PM

--- tictactoe.py
+++ tictactoe.py
@@ -1,1 +1,8 @@
-print('Hello Buster')
+from ggame import App, ImageAsset, RectangleAsset, Sprite, LineStyle, Frame, Color
+
+class Board(Sprite):
+    asset = ImageAsset("images/starfield.jpg")
+    width = 500
+    height = 500
+    def __init__(self, position):
+        super().__init__(Stars.asset, position)
