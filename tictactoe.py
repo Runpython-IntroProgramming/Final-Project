945824dfb1ba405d844ca10af1978ae78403934e
anEndrel23 Hhs <44811950+anEndrel23@users.noreply.github.com>
2019-05-08 13:54:41 -0400

    Updated from Brython Server: 5/8/2019 1:54:40 PM

--- tictactoe.py
+++ tictactoe.py
@@ -1,8 +1,17 @@
 from ggame import App, ImageAsset, RectangleAsset, Sprite, LineStyle, Frame, Color
 
 class Board(Sprite):
-    asset = ImageAsset("images/starfield.jpg")
-    width = 500
-    height = 500
+    asset = ImageAsset("images/Screen Shot 2019-05-07 at 2.54.32 PM.png")
     def __init__(self, position):
-        super().__init__(Stars.asset, position)
+        super().__init__(Board.asset, position)
+    Sprite(asset, (0,0))
+
+class SpaceGame(App):
+    def __init__(width, height):
+        super().__init__()
+        Board = Board((0,0))
+
+myapp = App()
+myapp.width = 1200
+myapp.height = 700
+myapp.run()
