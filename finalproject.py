2c38667750d8a3a9ffb3ef656e28952aca52150f
raLiinu30 <46948627+raLiinu30@users.noreply.github.com>
2019-05-08 13:57:12 -0400

    Updated from Brython Server: 5/8/2019 1:57:11 PM

--- finalproject.py
+++ finalproject.py
@@ -15,12 +15,14 @@ def step():
     if a < 1:
         oldxpen = (R-r)*cos(a) + r*50*cos(((R/r)-1)*a)+250
         oldypen = (R-r)*sin(a) - r*50*sin(((R/r)-1)*a)+240
+        #print("old: ", oldxpen, oldypen)
         a=a+0.05
         xpen = (R-r)*cos(a) + r*50*cos(((R/r)-1)*a)+250
         ypen = (R-r)*sin(a) - r*50*sin(((R/r)-1)*a)+240
+        #print("new: ", xpen, ypen)
         asset = CircleAsset(.5, thinline, black)
         asset2 = LineAsset(xpen-oldxpen, ypen-oldypen,thinline)
-        Sprite(asset, (xpen,ypen))
+        #Sprite(asset, (xpen,ypen))
         Sprite(asset2, (oldxpen, oldypen))
     
 myapp = App()
