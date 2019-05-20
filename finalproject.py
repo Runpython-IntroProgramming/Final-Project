0ca1ce81bbc31bf612adaac032d90e7283e785da
raLiinu30 <46948627+raLiinu30@users.noreply.github.com>
2019-05-20 14:30:15 -0400

    Updated from Brython Server: 5/20/2019 2:30:14 PM

--- finalproject.py
+++ finalproject.py
@@ -1,4 +1,8 @@
 # Rain's final project
+# Sources:
+# 1. https://math.stackexchange.com/questions/823578/how-do-i-calculate-the-perodicity-of-a-spirograph-hypotrochoid
+# 2. https://gist.github.com/endolith/114336/eff2dc13535f139d0d6a2db68597fad2826b53c3
+
 from math import sin, cos, pi
 from ggame import App, Color, LineStyle, Sprite, CircleAsset
 from ggame.line import LineSegment
@@ -9,11 +13,25 @@ thinline = LineStyle(1, black)
 R = float(input('Radius of circle 1: '))
 r = float(input('Radius of circle 2: '))
 
+R1 = 2*R*pi 
+r1 = 2*r*pi
+
+def gcd(R1,r1):
+    """Compute the greatest common divisor of a and b"""
+    while r1 > 0:
+        R1, r1 = r1, R1 % r1
+    return R1
+    
+def lcm(R1, r1):
+    """Compute the lowest common multiple of a and b"""
+    return R1 * r1 / gcd(R1, r1)
+
+
 a=0
 
 def step():
     global a
-    if a < 1500:
+    if a < lcm(R1,r1):
         oldxpen = (R-r)*cos(a) + r*1*cos(((R/r)-1)*a)+250
         oldypen = (R-r)*sin(a) - r*1*sin(((R/r)-1)*a)+240
         #print("old: ", oldxpen, oldypen)
@@ -25,9 +43,12 @@ def step():
         asset2 = LineSegment((oldxpen,oldypen), (xpen,ypen), positioning="physical")
         #Sprite(asset, (xpen,ypen))
 # when does the spirograph repeat, integer numbers of R and r
+# spirograph repeats when a hits the lcm of both radii
 # style; line style
 #rainbow spirograph
 # go through once, then delete and add 2 shorter lines, again and again 
+
+
     
 myapp = App()
 myapp.run(step)
