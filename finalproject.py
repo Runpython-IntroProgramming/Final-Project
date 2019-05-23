3a678bd7b107885add6268c7f3ec115036a76e99
raLiinu30 <46948627+raLiinu30@users.noreply.github.com>
2019-05-23 14:33:55 -0400

    Updated from Brython Server: 5/23/2019 2:33:54 PM

--- finalproject.py
+++ finalproject.py
@@ -9,38 +9,44 @@ from ggame.line import LineSegment
 
 black = Color(0x000000, 1.0)
 thinline = LineStyle(1, black)
+darkorange1 = Color(0xD07702, 0)
+thinline4 = LineStyle(1, darkorange)
+
+listc = [thinline, thinline4]
 
 R = float(input('Radius of circle 1: '))
 r = float(input('Radius of circle 2: '))
 
-R1 = 2*R*pi 
-r1 = 2*r*pi
-
-def gcd(R1,r1):
+def gcd(R,r):
     """Compute the greatest common divisor of a and b"""
-    while r1 > 0:
-        R1, r1 = r1, R1 % r1
-    return R1
+    while r > 0:
+        R, r = r, R % r
+    return R
     
-def lcm(R1, r1):
+def lcm(R, r):
     """Compute the lowest common multiple of a and b"""
-    return R1 * r1 / gcd(R1, r1)
+    return R * r / gcd(R, r)
 
 
 a=0
+stylecount = 0
 
 def step():
     global a
-    if a < lcm(R1,r1):
+    if a < (2*pi*lcm(R,r))/R:
         oldxpen = (R-r)*cos(a) + r*1*cos(((R/r)-1)*a)+250
         oldypen = (R-r)*sin(a) - r*1*sin(((R/r)-1)*a)+240
-        #print("old: ", oldxpen, oldypen)
         a=a+0.2
         xpen = (R-r)*cos(a) + r*1*cos(((R/r)-1)*a)+250
         ypen = (R-r)*sin(a) - r*1*sin(((R/r)-1)*a)+240
-        #print("new: ", xpen, ypen)
         asset = CircleAsset(.5, thinline, black)
-        asset2 = LineSegment((oldxpen,oldypen), (xpen,ypen), positioning="physical")
+        stylecount += 1
+        asset2 = LineSegment((oldxpen,oldypen), (xpen,ypen), style = listc[stylecount], positioning="physical")
+        
+        
+         #print("new: ", xpen, ypen)
+        
+         #print("old: ", oldxpen, oldypen)
         #Sprite(asset, (xpen,ypen))
 # when does the spirograph repeat, integer numbers of R and r
 # spirograph repeats when a hits the lcm of both radii
