f0cf67d9c1375096d1e7980a85105981cad98e8a
waSclthu11 <46973645+waSclthu11@users.noreply.github.com>
2019-05-28 14:52:52 -0400

    Updated from Brython Server: 5/28/2019 2:52:51 PM

--- finalproject.py
+++ finalproject.py
@@ -344,7 +344,7 @@ class Variblock(Sprite):
     def __init__(self, w, h, x, y):
         grid=lambda W: (W-W%10)
         gred=lambda W: (W*10)
-        asset = ImageAsset("images/MakingMap1.png", Frame(0,0,gred(w),gred(h)), 1, 'horizontal')
+        asset = ImageAsset("images/casssstle.jpg", Frame(0,0,gred(w),gred(h)), 1, 'horizontal')
         #super().__init__(RectangleAsset(gred(w),gred(h),noline,grey),(grid(x),grid(y)))
         super().__init__(asset,(grid(x),grid(y)))
 
