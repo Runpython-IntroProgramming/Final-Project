b06f0407f7aea1a5a6d2a13eacba96089e75d482
kyDoleuc04 <46973452+kyDoleuc04@users.noreply.github.com>
2019-05-28 17:52:23 -0400

    Updated from Brython Server: 5/28/2019 5:52:24 PM

--- final.py
+++ final.py
@@ -16,7 +16,7 @@ class Bird(Sprite):
     side = LineStyle(1,black)
     poly = RectangleAsset(10,10, side, yellow)
     def __init__(self,position):
-        super().__init__(Person.poly,position)
+        super().__init__(Bird.poly,position)
         self.vx = 0
         self.vy = 0
 
@@ -29,9 +29,7 @@ class Block(Sprite):
 class Game(App):
     def __init__(self):
         super().__init__(1000,540)
-        
-        self.gameover = False
-        
+    
         Game.listenKeyEvent('keydown', 'right arrow',  self.right)
         Game.listenKeyEvent('keyup', 'right arrow',  self.rightstop)
         Game.listenKeyEvent('keydown', 'left arrow',  self.left)
@@ -39,48 +37,50 @@ class Game(App):
         Game.listenKeyEvent('keydown', 'up arrow',  self.up)
         Game.listenKeyEvent('keyup', 'up arrow',  self.upstop)
         Game.listenKeyEvent('keydown', 'down arrow',  self.down)
-        Game.listenKeyEvent('keyup', 'down arrow',  self.downstop)
+        Game.listenKeyEvent('keyup', 'down arrow',  self.downstop)   
         
         x = 0
         y = 0
         
-        Bird(10,250)
-        Block(75,100)
-        Block(75,130)
-        Block(75,160)
+        Bird((10,250))
+        Block((75,100))
+        Block((75,130))
+        Block((75,160))
         
     def right(self,event):
-        for sprite in self.getSpritesbyClass(Person):
+        for sprite in self.getSpritesbyClass(Bird):
             sprite.vx = 2
             
     def rightstop(self,event):
-        for sprite in self.getSpritesbyClass(Person):
+        for sprite in self.getSpritesbyClass(Bird):
             sprite.vx = 0
             
     def left(self,event):
-        for sprite in self.getSpritesbyClass(Person):
+        for sprite in self.getSpritesbyClass(Bird):
             sprite.vx = -2
             
     def leftstop(self,event):
-        for sprite in self.getSpritesbyClass(Person):
+        for sprite in self.getSpritesbyClass(Bird):
             sprite.vx = 0
     
     def up(self,event):
         #if self.grounded == True: 
-            for sprite in self.getSpritesbyClass(Person):
+            for sprite in self.getSpritesbyClass(Bird):
                 sprite.vy = -5
     
     def upstop(self,event):
-        for sprite in self.getSpritesbyClass(Person):
+        for sprite in self.getSpritesbyClass(Bird):
             sprite.vy = 0
             
     def down(self,event):
-        for sprite in self.getSpritesbyClass(Person):
+        for sprite in self.getSpritesbyClass(Bird):
             sprite.vy = 2
     
     def downstop(self,event):
-        for sprite in self.getSpritesbyClass(Person):
+        for sprite in self.getSpritesbyClass(Bird):
             sprite.vy = 0
+            
+   
 
 myapp = Game()
 myapp.run()
