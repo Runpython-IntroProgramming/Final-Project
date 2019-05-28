f6880a357cd551b09dc38b17d857d94a5c265cda
anEndrel23 Hhs <44811950+anEndrel23@users.noreply.github.com>
2019-05-28 17:35:52 -0400

    Updated from Brython Server: 5/28/2019 5:35:49 PM

--- tictactoe.py
+++ tictactoe.py
@@ -45,17 +45,6 @@ class diag2(Sprite):
         self.rotation = -.785
         
 
-a = 1
-b = 1
-c = 1
-d = 1
-e = 1
-f = 1
-g = 1
-h = 1
-i = 1
-
-
 class Game(App):
     def __init__(self):
         super().__init__()
@@ -91,130 +80,131 @@ class Game(App):
         self.s7 = 1
         self.s8 = 1
         self.s9 = 1
+        self.winner = 0
     def spot1(self, event):
-        if self.turns % 2 == 0 and self.if1 == 0:
+        if self.turns % 2 == 0 and self.if1 == 0 and self.winner == 0:
             exe((300,200))
             self.s1 = 5
             self.turns += 1
             self.if1 = 1
-        elif self.turns % 2 == 1 and self.if1 == 0:
+        elif self.turns % 2 == 1 and self.if1 == 0 and self.winner == 0:
             owe((300,200))
             self.s1 = 7
             self.turns += 1
             self.if1 = 1
-        else:
+        elif self.winner == 0:
             print("Please choose another space")
         self.checkwinner()
     def spot2(self, event):
-        if self.turns % 2 == 0 and self.if2 == 0:
+        if self.turns % 2 == 0 and self.if2 == 0 and self.winner == 0:
             exe((450,200))
             self.s2 = 5
             self.turns +=1
             self.if2 = 1
-        elif self.turns % 2 == 1 and self.if2 == 0:
+        elif self.turns % 2 == 1 and self.if2 == 0 and self.winner == 0:
             owe((450,200))
             self.s2 = 7
             self.turns +=1
             self.if2 = 1
-        else:
+        elif self.winner == 0:
             print("Please choose another space")
         self.checkwinner()
     def spot3(self, event):
-        if self.turns % 2 == 0 and self.if3 == 0:
+        if self.turns % 2 == 0 and self.if3 == 0 and self.winner == 0:
             exe((600,200))
             self.s3 = 5
             self.turns += 1
             self.if3 = 1
-        elif self.turns % 2 == 1 and self.if3 == 0:
+        elif self.turns % 2 == 1 and self.if3 == 0 and self.winner == 0:
             owe((600,200))
             self.s3 = 7
             self.turns += 1
             self.if3 = 1
-        else:
+        elif self.winner == 0:
             print("Please choose another space")
         self.checkwinner()
     def spot4(self, event):
-        if self.turns % 2 == 0 and self.if4 == 0:
+        if self.turns % 2 == 0 and self.if4 == 0 and self.winner == 0:
             exe((300,350))
             self.s4 = 5
             self.turns += 1
             self.if4 = 1
-        elif self.turns % 2 == 1 and self.if4 == 0:
+        elif self.turns % 2 == 1 and self.if4 == 0 and self.winner == 0:
             owe((300,350))
             self.s4 = 7
             self.turns += 1
             self.if4 = 1
-        else:
+        elif self.winner == 0:
             print("Please choose another space")
         self.checkwinner()
     def spot5(self, event):
-        if self.turns % 2 == 0 and self.if5 == 0:
+        if self.turns % 2 == 0 and self.if5 == 0 and self.winner == 0:
             exe((450,350))
             self.s5 = 5
             self.turns += 1
             self.if5 = 1
-        elif self.turns % 2 == 1 and self.if5 == 0:
+        elif self.turns % 2 == 1 and self.if5 == 0 and self.winner == 0:
             owe((450,350))
             self.s5 = 7
             self.turns += 1
             self.if5 = 1
-        else:
+        elif self.winner == 0:
             print("Please choose another space")
         self.checkwinner()
     def spot6(self, event):
-        if self.turns % 2 == 0 and self.if6 == 0:
+        if self.turns % 2 == 0 and self.if6 == 0 and self.winner == 0:
             exe((600,350))
             self.s6 = 5
             self.turns += 1
             self.if6 = 1
-        elif self.turns % 2 == 1 and self.if6 == 0:
+        elif self.turns % 2 == 1 and self.if6 == 0 and self.winner == 0:
             owe((600,350))
             self.s6 = 7
             self.turns += 1
             self.if6 = 1
-        else:
+        elif self.winner == 0:
             print("Please choose another space")
         self.checkwinner()
     def spot7(self, event):
-        if self.turns % 2 == 0 and self.if7 == 0:
+        if self.turns % 2 == 0 and self.if7 == 0 and self.winner == 0:
             exe((300,500))
             self.s7 = 5
             self.turns += 1
             self.if7 = 1
-        elif self.turns % 2 == 1 and self.if7 == 0:
+        elif self.turns % 2 == 1 and self.if7 == 0 and self.winner == 0:
             owe((300,500))
             self.s7 = 7
             self.turns += 1
             self.if7 = 1
-        else:
+        elif self.winner == 0:
             print("Please choose another space")
         self.checkwinner()
     def spot8(self, event):
-        if self.turns % 2 == 0 and self.if8 == 0:
+        if self.turns % 2 == 0 and self.if8 == 0 and self.winner == 0:
             exe((450,500))
             self.s8 = 5
             self.turns += 1
             self.if8 = 1
-        elif self.turns % 2 == 1 and self.if8 == 0:
+        elif self.turns % 2 == 1 and self.if8 == 0 and self.winner == 0:
             owe((450,500))
             self.s8 = 7
             self.turns += 1
             self.if8 = 1
-        else:
+        elif self.winner == 0:
             print("Please choose another space")
         self.checkwinner()
     def spot9(self, event):
-        if self.turns % 2 == 0 and self.if9 == 0:
+        if self.turns % 2 == 0 and self.if9 == 0 and self.winner == 0:
             exe((600,500))
             self.s9 = 5
             self.turns += 1
             self.if9 = 1
-        elif self.turns % 2 == 1 and self.if9 == 0:
+        elif self.turns % 2 == 1 and self.if9 == 0 and self.winner == 0:
             owe((600,500))
             self.s9 = 7
             self.turns += 1
             self.if9 = 1
-        else:
+        elif self.winner == 0:
             print("Please choose another space")
         self.checkwinner()
 
@@ -222,70 +212,88 @@ class Game(App):
         if self.s1 == 5 and self.s2 == 5 and self.s3 == 5:
             horiz((270,230))
             print("X Wins")
+            self.winner = 1
             return
         elif self.s4 == 5 and self.s5 == 5 and self.s6 == 5:
             horiz((270,380))
             print("X Wins")
+            self.winner = 1
             return
         elif self.s7 == 5 and self.s8 == 5 and self.s9 == 5:
             horiz((270,530))
             print("X Wins")
+            self.winner = 1
             return
         elif self.s1 == 5 and self.s4 == 5 and self.s7 == 5:
             vert((340,160))
             print("X Wins")
+            self.winner = 1
             return
         elif self.s2 == 5 and self.s5 == 5 and self.s8 == 5:
             vert((490,160))
             print("X Wins")
+            self.winner = 1
             return
         elif self.s3 == 5 and self.s6 == 5 and self.s9 == 5:
             vert((640,160))
             print("X Wins")
+            self.winner = 1
             return
         elif self.s1 == 5 and self.s5 == 5 and self.s9 == 5:
             diag1((270,160))
             print ("X Wins")
+            self.winner = 1
             return
         elif self.s3 == 5 and self.s5 == 5 and self.s7 == 5:
             diag2((720,160))
             print ("X Wins")
+            self.winner = 1
             return
         elif self.s1 == 7 and self.s2 == 7 and self.s3 == 7:
             horiz((303,250))
             print("O Wins")
+            self.winner = 1
             return
         elif self.s4 == 7 and self.s5 == 7 and self.s6 == 7:
             horiz((303,400))
             print("O Wins")
+            self.winner = 1
             return
         elif self.s7 == 7 and self.s8 == 7 and self.s9 == 7:
             horiz((303,550))
             print("O Wins")
+            self.winner = 1
             return
         elif self.s1 == 7 and self.s4 == 7 and self.s7 == 7:
             vert((303,400))
             print("O Wins")
+            self.winner = 1
             return
         elif self.s2 == 7 and self.s5 == 7 and self.s8 == 7:
             vcert((303,400))
             print("O Wins")
+            self.winner = 1
             return
         elif self.s3 == 7 and self.s6 == 7 and self.s9 == 7:
             horiz((303,400))
             print("O Wins")
+            self.winner = 1
             return
         elif self.s1 == 7 and self.s5 == 7 and self.s9 == 7:
             diag1((303, 250))
             print ("O Wins")
+            self.winner = 1
             return
         elif self.s3 == 7 and self.s5 == 7 and self.s7 == 7:
             diag2((603, 250))
             print ("O Wins")
+            self.winner = 1
             return
         elif self.if1 == 1 and self.if2 == 1 and self.if3 == 1 and self.if4 == 1 and self.if5 == 1 and self.if6 == 1 and self.if7 == 1 and self.if8 == 1 and self.if9 == 1:
-            print("Draw")
+            print("Draw. Press Enter to Reset")
+            self.winner = 1
             return
+        
 
 myapp = Game()
 myapp.run()
