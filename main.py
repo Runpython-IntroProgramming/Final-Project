e52023a2ed8e2ce374b3aec4a2004e4bbd80eb89
Spyglass117 <46973734+Spyglass117@users.noreply.github.com>
2019-05-29 09:24:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import string
 import random
+from browser.local_storage import storage
 
 #Defining variables
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,:;'\"/\\<>(){}[]-=_+?!"
@@ -76,20 +77,20 @@ def resumelist(code):
         data.append(password)
         sitedata.append(data)
         del code1[0]
-        del code1
+        del code1[0]
         halflencode -= 1
         x += 1
 
     return(sitedata)
     
-command = input("Welcome! Press r to resume a session, press s to store a site and password, press g to retrieve a site and password or press q to print and exit: ")
+command = input("Welcome! Press s to store a site and password, press g to retrieve a site and password, press c to clear stored passwords, or press q to exit: ")
+resumecodeentered = storage['pstr']
+sitedata = resumelist(resumecodeentered)
+key = storage['key']
 while running == 0:
     
-    if command == "r":
-        resumecodeentered = input("Please enter your resume code: ")
-        sitedata = resumelist(resumecodeentered)
-        command = input("Press r to resume a session, press s to store a site and password, press g to retrieve a site and password or press q to print and exit: ")
-    elif command == "s":
+
+    if command == "s":
         data = []
         site = input("Please enter the name of the site you wish to store: ")
         password = input("Please enter the password belonging to the site: ")
@@ -100,7 +101,7 @@ while running == 0:
         sitedata.append(data)
         code = sitedata.index(data)
         print ("Your site code is: {0}. Please write this down and store it in a safe place so you can access it later.".format(code))
-        command = input("Press r to resume a session, press s to store a site and password, press g to retrieve a site and password or press q to print and exit: ")
+        command = input("Press s to store a site and password, press g to retrieve a site and password, press c to clear stored passwords, or press q to exit: ")
 
 
     elif command == "g":
@@ -117,8 +118,150 @@ while running == 0:
         site = decrypt(site)
         password = decrypt(password)
         print("Your password for {0} is {1}.".format(site, password))
-        command = input("Press r to resume a session, press s to store a site and password, press g to retrieve a site and password or press q to print and exit: ")
+        command = input("Press s to store a site and password, press g to retrieve a site and password, press c to clear stored passwords or press q to exit: ")
+        
+    elif command == "c":
+        
+        runcheck = 0
+        
+        while runcheck == 0:
+            import string
+import random
+from browser.local_storage import storage
 
+#Defining variables
+associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,:;'\"/\\<>(){}[]-=_+?!"
+str1 = ""
+
+keylen = ""
+keylen += str(random.randint(1, 9))
+keylen += str(random.randint(0, 9))
+keylen  = int(keylen)
+
+key = ""
+key1 = ""
+counter = 1
+while counter <= keylen:
+    key += key1.join(random.choice(associations))
+    counter += 1
+
+
+sitedata = []
+running = 0
+
+
+
+# Function for encrypting a string
+def encrypt(message):
+    global associations
+    global key
+    str1 = ""
+    n = 0
+    while len(key) < len(message):
+        key += key
+    associations += associations
+    l = len(message)
+    while n < l:
+        numrepm = associations.find(message[n])
+        numrepk = associations.find(key[n])
+        finalnum = numrepm + numrepk
+        letrep = associations[finalnum]
+        str1 = str1 + letrep
+        n = n + 1
+    return(str1)
+  
+# Function for decrypting a string  
+def decrypt(message):
+    global associations
+    global key
+    str2 = ""
+    n = 0
+    while len(key) < len(message):
+        key += key
+    associations += associations
+    l = len(message)
+    while n < l:
+        numrepm1 = associations.find(message[n])
+        numrepk1 = associations.find(key[n])
+        finalnum1 = numrepm1 - numrepk1
+        letrep1 = associations[finalnum1]
+        str2 = str2 + letrep1
+        n = n + 1
+    return(str2)
+    
+# Function for converting an entered string to a list
+def resumelist(code):
+    global sitedata
+    code1 = code.split()
+    lencode = len(code1)
+    halflencode = lencode/2
+    x = 0
+    while x <= halflencode:
+        site = code1[0]
+        password = code1[1]
+        data = []
+        data.append(site)
+        data.append(password)
+        sitedata.append(data)
+        del code1[0]
+        del code1[0]
+        halflencode -= 1
+        x += 1
+
+    return(sitedata)
+    
+command = input("Welcome! Press s to store a site and password, press g to retrieve a site and password, press c to clear stored passwords, or press q to exit: ")
+resumecodeentered = storage['pstr']
+sitedata = resumelist(resumecodeentered)
+key = storage['key']
+while running == 0:
+    
+
+    if command == "s":
+        data = []
+        site = input("Please enter the name of the site you wish to store: ")
+        password = input("Please enter the password belonging to the site: ")
+        site = encrypt(site)
+        password = encrypt(password)
+        data.append(site)
+        data.append(password)
+        sitedata.append(data)
+        code = sitedata.index(data)
+        print ("Your site code is: {0}. Please write this down and store it in a safe place so you can access it later.".format(code))
+        command = input("Press s to store a site and password, press g to retrieve a site and password, press c to clear stored passwords, or press q to exit: ")
+
+
+    elif command == "g":
+        code = input("Please enter the code of the site you wish to retrieve: ")
+        try:
+            code = int(code)
+        except ValueError:
+            print("Sorry, code not recognized, please try again.")
+        code = int(code)
+        
+        data = sitedata[code]
+        site = data[0]
+        password = data[1]
+        site = decrypt(site)
+        password = decrypt(password)
+        print("Your password for {0} is {1}.".format(site, password))
+        command = input("Press s to store a site and password, press g to retrieve a site and password, press c to clear stored passwords or press q to exit: ")
+        
+    elif command == "c":
+        
+        runcheck = 0
+        
+        while runcheck == 0:
+            confirm = input("Are you sure you want to clear local data? Y or N.")
+            if confirm == "Y":
+                del storage['key']
+                del storage['pstr']
+                runcheck = 1
+            elif confirm == "N":
+                command = input("Understood. Press s to store a site and password, press g to retrieve a site and password, press c to clear stored passwords or press q to exit: ")
+                runcheck = 1
+            else:
+                print("Sorry, command not recognized, please try again.")
         
     elif command == "q":
         printstring = ""
@@ -127,9 +270,35 @@ while running == 0:
             printstring = printstring + " "
             printstring = printstring + p[1]
             printstring = printstring + " "
-        print("Your resume code is {0}. Please write it down and keep it in a safe place so you can access your data later.".format(printstring))
-        print("Your ket is {0}. Please write it down and keep it in a safe place so you can access your data later.".format(key))
+        storage['pstr'] = printstring
+        storage['key'] = key
+        print("Goodbye!")
         running = 1
     else:
         print("Sorry, command not understood. Please try again.")
-        command = input("Press r to resume a session, press s to store a site and password, press g to retrieve a site and password or press q to print and exit: ")
+        command = input("Press s to store a site and password, press g to retrieve a site and password, press c to clear stored passwords or press q to exit: ")
+
+            if confirm == "Y":
+                del storage['key']
+                del storage['pstr']
+                runcheck = 1
+            elif confirm == "N":
+                command = input("Understood. Press s to store a site and password, press g to retrieve a site and password, press c to clear stored passwords or press q to exit: ")
+                runcheck = 1
+            else:
+                print("Sorry, command not recognized, please try again.")
+        
+    elif command == "q":
+        printstring = ""
+        for p in sitedata:
+            printstring = printstring + p[0]
+            printstring = printstring + " "
+            printstring = printstring + p[1]
+            printstring = printstring + " "
+        storage['pstr'] = printstring
+        storage['key'] = key
+        print("Goodbye!")
+        running = 1
+    else:
+        print("Sorry, command not understood. Please try again.")
+        command = input("Press s to store a site and password, press g to retrieve a site and password, press c to clear stored passwords or press q to exit: ")
